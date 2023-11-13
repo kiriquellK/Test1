ed899f1e33f9addd86a8013ae402f1d43c53e4e9
kiriquellK <kiryaep@gmail.com>
2023-11-13 22:04:18 +0300

    Добавьте файлы проекта.

---
+++ Test1/MyStack.cs
@@ -0,0 +1,45 @@
+﻿namespace Classes
+{
+    internal class MyStack
+    {
+        private int _count;
+        private int _item;
+        private int[] _items;
+        
+
+        private int Item
+        {
+            get
+                {
+                return _item;
+            }
+            set             
+            {
+                _item = value;
+            }
+        }
+        private int Count
+        {
+            get
+            {
+                return _item;
+            }
+            set
+            {
+                _item = value;
+            }
+        }
+
+        public MyStack()
+        { }
+        public MyStack(int count)
+        {
+            _count = count;
+        }
+
+        public void Push()
+        {
+            
+        }
+    }
+}
---
+++ Test1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Classes
+{
+    internal class Program
+    {
+        public static void Main(string[] args) 
+        {
+            
+        }
+    }
+}
